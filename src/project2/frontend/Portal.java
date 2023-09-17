96eb6736ddc5649d316968ec51d0d00ce42f4249
Johan Rickardo A. Roxas <2233375@slu.edu.ph>
2023-09-17 23:09:08 +0800

    Added new icons, GUI components for Schedule, and initial version of TOR for Portal.java, and other layouting

--- src/project2/frontend/Portal.java
+++ src/project2/frontend/Portal.java
@@ -2,6 +2,8 @@ package project2.frontend;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author TBA
@@ -14,6 +16,41 @@ public class Portal extends JFrame {
      * An object of Resources that is used to invoke UI resources and elements.
      */
     private final Resources resources = new Resources();
+
+    /**
+     * TODO: Documentation
+     */
+    private JButton homeButton;
+
+    /**
+     * TODO: Documentation
+     */
+    private JButton scheduleButton;
+
+    /**
+     * TODO: Documentation
+     */
+    private JButton torButton;
+
+    /**
+     * TODO: Documentation
+     */
+    private JButton checklistButton;
+
+    /**
+     * TODO: Documentation
+     */
+    private JButton personalDetailsButton;
+
+    /**
+     * Card Layout used for the centerPanel
+     */
+    private final CardLayout cardLayout1 = new CardLayout(10,20);
+
+    /**
+     * Card Layout used for components inside centerPanel
+     */
+    private final CardLayout cardLayout2 = new CardLayout(10,20);
 
     /**
      * Constructs an object of Portal.
@@ -36,7 +73,6 @@ public class Portal extends JFrame {
 
         // ! Sidebar Panel
         JPanel sidebarPanel = populateSidebar(); // Holds the sidebar
-        sidebarPanel.setPreferredSize(new Dimension(260,700));
         mainPanel.add(sidebarPanel, BorderLayout.WEST);
 
         // ! Center Panel
@@ -44,6 +80,74 @@ public class Portal extends JFrame {
         centerPanel.setLayout(new BorderLayout());
         centerPanel.setBackground(resources.antiflashWhite);
         mainPanel.add(centerPanel, BorderLayout.CENTER);
+
+        // !! Center Panel Components
+
+        // !! Card Panel
+        JPanel cardPanel = new JPanel();
+        cardPanel.setLayout(cardLayout1);
+        cardPanel.setBackground(resources.antiflashWhite);
+        centerPanel.add(cardPanel);
+
+        // !!! Card Panel Components
+
+        // !!! Home Panel
+        JPanel homePanel = populateHomePanel();
+        cardPanel.add(homePanel, "home");
+
+        // !!! Schedule Panel
+        JPanel schedulePanel = populateSchedulePanel();
+        cardPanel.add(schedulePanel, "schedule");
+
+        // !!! TOR Panel
+        JPanel torPanel = populateTorPanel();
+        cardPanel.add(torPanel, "tor");
+
+        // Action Listeners
+        homeButton.addActionListener(e -> {
+            cardLayout1.show(cardPanel, "home");
+            homeButton.setForeground(resources.antiflashWhite);
+            scheduleButton.setForeground(Color.BLACK);
+            torButton.setForeground(Color.BLACK);
+            checklistButton.setForeground(Color.BLACK);
+            personalDetailsButton.setForeground(Color.BLACK);
+        });
+        scheduleButton.addActionListener(e -> {
+            cardLayout1.show(cardPanel, "schedule");
+            homeButton.setForeground(Color.BLACK);
+            scheduleButton.setForeground(resources.antiflashWhite);
+            torButton.setForeground(Color.BLACK);
+            checklistButton.setForeground(Color.BLACK);
+            personalDetailsButton.setForeground(Color.BLACK);
+        });
+        torButton.addActionListener(e -> {
+            cardLayout1.show(cardPanel, "tor");
+            cardLayout1.show(torPanel, "1");
+            homeButton.setForeground(Color.BLACK);
+            scheduleButton.setForeground(Color.BLACK);
+            torButton.setForeground(resources.antiflashWhite);
+            checklistButton.setForeground(Color.BLACK);
+            personalDetailsButton.setForeground(Color.BLACK);
+        });
+        checklistButton.addActionListener(e -> {
+            cardLayout1.show(cardPanel, "checklist");
+            homeButton.setForeground(Color.BLACK);
+            scheduleButton.setForeground(Color.BLACK);
+            torButton.setForeground(Color.BLACK);
+            checklistButton.setForeground(resources.antiflashWhite);
+            personalDetailsButton.setForeground(Color.BLACK);
+        });
+        personalDetailsButton.addActionListener(e -> {
+            cardLayout1.show(cardPanel, "personalDetails");
+            cardLayout1.show(cardPanel, "checklist");
+            homeButton.setForeground(Color.BLACK);
+            scheduleButton.setForeground(Color.BLACK);
+            torButton.setForeground(Color.BLACK);
+            checklistButton.setForeground(Color.BLACK);
+            personalDetailsButton.setForeground(resources.antiflashWhite);
+        });
+
+
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(1200,700);
@@ -155,12 +259,12 @@ public class Portal extends JFrame {
         // !! Home Button
         gbc.gridy = 1;
         ImageIcon homeIcon = new ImageIcon("icons/home-icon-black.png");
-        JButton homeButton = new JButton();
+        homeButton = new JButton();
         homeButton.setText("Home");
         homeButton.setIcon(homeIcon);
         homeButton.setForeground(resources.antiflashWhite);
         homeButton.setHorizontalAlignment(SwingConstants.LEFT);
-        homeButton.setFont(resources.montserratBold.deriveFont(12f));
+        homeButton.setFont(resources.montserratBold.deriveFont(15f));
         homeButton.setBorderPainted(true);
         homeButton.setOpaque(false);
         homeButton.setContentAreaFilled(false);
@@ -169,11 +273,11 @@ public class Portal extends JFrame {
         // !! Schedule Button
         gbc.gridy = 2;
         ImageIcon scheduleIcon = new ImageIcon("icons/schedule-icon-black.png");
-        JButton scheduleButton = new JButton();
+        scheduleButton = new JButton();
         scheduleButton.setText("Schedule");
         scheduleButton.setIcon(scheduleIcon);
         scheduleButton.setHorizontalAlignment(SwingConstants.LEFT);
-        scheduleButton.setFont(resources.montserratBold.deriveFont(12f));
+        scheduleButton.setFont(resources.montserratBold.deriveFont(15f));
         scheduleButton.setBorderPainted(false);
         scheduleButton.setOpaque(false);
         scheduleButton.setContentAreaFilled(false);
@@ -182,11 +286,11 @@ public class Portal extends JFrame {
         // !! Attendance Button
         gbc.gridy = 3;
         ImageIcon torIcon = new ImageIcon("icons/tor-icon-black.png");
-        JButton torButton = new JButton();
+        torButton = new JButton();
         torButton.setText("Transcript of Records");
         torButton.setIcon(torIcon);
         torButton.setHorizontalAlignment(SwingConstants.LEFT);
-        torButton.setFont(resources.montserratBold.deriveFont(12f));
+        torButton.setFont(resources.montserratBold.deriveFont(15f));
         torButton.setBorderPainted(false);
         torButton.setOpaque(false);
         torButton.setContentAreaFilled(false);
@@ -195,11 +299,11 @@ public class Portal extends JFrame {
         // !! Statement of Accounts Button
         gbc.gridy = 4;
         ImageIcon checklistIcon = new ImageIcon("icons/checklist-icon-black.png");
-        JButton checklistButton = new JButton();
+        checklistButton = new JButton();
         checklistButton.setText("Curriculum Checklist");
         checklistButton.setIcon(checklistIcon);
         checklistButton.setHorizontalAlignment(SwingConstants.LEFT);
-        checklistButton.setFont(resources.montserratBold.deriveFont(12f));
+        checklistButton.setFont(resources.montserratBold.deriveFont(15f));
         checklistButton.setBorderPainted(false);
         checklistButton.setOpaque(false);
         checklistButton.setContentAreaFilled(false);
@@ -208,19 +312,242 @@ public class Portal extends JFrame {
         // !! Personal Details Button
         gbc.gridy = 5;
         ImageIcon personalIcon = new ImageIcon("icons/personal-icon-black.png");
-        JButton personalButton = new JButton();
-        personalButton.setText("Personal Details");
-        personalButton.setIcon(personalIcon);
-        personalButton.setHorizontalAlignment(SwingConstants.LEFT);
-        personalButton.setFont(resources.montserratBold.deriveFont(12f));
-        personalButton.setBorderPainted(false);
-        personalButton.setOpaque(false);
-        personalButton.setContentAreaFilled(false);
-        navPanel.add(personalButton, gbc);
+        personalDetailsButton = new JButton();
+        personalDetailsButton.setText("Personal Details");
+        personalDetailsButton.setIcon(personalIcon);
+        personalDetailsButton.setHorizontalAlignment(SwingConstants.LEFT);
+        personalDetailsButton.setFont(resources.montserratBold.deriveFont(15f));
+        personalDetailsButton.setBorderPainted(false);
+        personalDetailsButton.setOpaque(false);
+        personalDetailsButton.setContentAreaFilled(false);
+        navPanel.add(personalDetailsButton, gbc);
 
         return sidebarPanel;
     } // end of populateSidebar method
 
+    /**
+     * TODO: Documentation
+     * @return
+     */
+    private JPanel populateHomePanel() {
+        // Home Panel
+        JPanel homePanel = new JPanel();
+        homePanel.setLayout(new GridLayout(1, 2));
+        homePanel.setBorder(resources.normalPadding);
+        homePanel.setBackground(resources.antiflashWhite);
+        homePanel.setPreferredSize(new Dimension(910, 700));
+
+        // ! Home Panel Components
+
+        // ! Bulletin Board Panel
+        JPanel bulletinPanel = new JPanel();
+        bulletinPanel.setLayout(new GridLayout(5,1));
+        bulletinPanel.setBorder(resources.normalPadding);
+        bulletinPanel.setBackground(resources.uranianBlue);
+        homePanel.add(bulletinPanel);
+
+        // !! Bulletin Board Components
+        // TODO: Supporting code
+
+        // ! Student Panel
+        JPanel studentPanel = new JPanel();
+        studentPanel.setLayout(new BorderLayout());
+        studentPanel.setBorder(resources.normalPadding);
+        studentPanel.setBackground(resources.battleshipGray);
+        homePanel.add(studentPanel);
+
+        // !! Student Panel Components
+        // TODO: Supporting code
+
+        return homePanel;
+    } // end of populateHomePanel method
+
+    /**
+     * TODO: Documentation
+     * @return
+     */
+    private JPanel populateSchedulePanel() {
+        JPanel schedulePanel = new JPanel();
+        schedulePanel.setLayout(new BorderLayout());
+        schedulePanel.setBackground(resources.antiflashWhite);
+        schedulePanel.setPreferredSize(new Dimension(910, 700));
+
+        JLabel scheduleLabel = new JLabel();
+        scheduleLabel.setText("Not Available.");
+        scheduleLabel.setFont(resources.montserratBold.deriveFont(50f));
+        scheduleLabel.setVerticalAlignment(SwingConstants.CENTER);
+        scheduleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        scheduleLabel.setForeground(Color.BLACK);
+        schedulePanel.add(scheduleLabel, BorderLayout.CENTER);
+
+        JLabel contactLabel = new JLabel();
+        contactLabel.setText("Contact the development team for further details.");
+        contactLabel.setFont(resources.montserratBold.deriveFont(20f));
+        contactLabel.setVerticalAlignment(SwingConstants.CENTER);
+        contactLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        contactLabel.setForeground(Color.BLACK);
+        schedulePanel.add(contactLabel, BorderLayout.SOUTH);
+
+        return schedulePanel;
+    } // end of populateSchedulePanel method
+
+    private JPanel populateTorPanel() {
+        // Transcript of Records Panel
+        JPanel torPanel = new JPanel();
+        torPanel.setLayout(cardLayout1);
+        torPanel.setBorder(resources.thinPadding);
+        torPanel.setBackground(resources.antiflashWhite);
+        torPanel.setPreferredSize(new Dimension(910, 700));
+
+        // ! TOR Components
+        
+        // ! Container 1
+        JPanel containerPanel1 = new JPanel();
+        containerPanel1.setLayout(new BorderLayout());
+        containerPanel1.setBackground(resources.antiflashWhite);
+        containerPanel1.setPreferredSize(new Dimension(910,700));
+        torPanel.add(containerPanel1, "1");
+        
+        // !! Container 1 Components
+        
+        // !! Search Panel
+        JPanel searchPanel = new JPanel();
+        searchPanel.setLayout(new GridBagLayout());
+        searchPanel.setBorder(resources.normalPadding);
+        searchPanel.setBackground(Color.darkGray);
+        searchPanel.setPreferredSize(new Dimension(800,600));
+        containerPanel1.add(searchPanel, BorderLayout.CENTER);
+        
+        // !!! Search Panel Components
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5,20,5,20);
+        gbc.weightx = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        // !!! Student Panel
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+
+        JPanel studentPanel = new JPanel();
+        studentPanel.setLayout(new BorderLayout());
+        studentPanel.setBackground(Color.darkGray);
+        studentPanel.setPreferredSize(new Dimension(800, 200));
+        searchPanel.add(studentPanel, gbc);
+
+        // !!!! Student Panel Components
+        ImageIcon studentIcon = new ImageIcon("icons/student-icon-black.png");
+
+        JLabel studentLabel = new JLabel();
+        studentLabel.setText("LAST NAME, FIRST NAME");
+        studentLabel.setIcon(studentIcon);
+        studentLabel.setFont(resources.montserratBold.deriveFont(15f));
+        studentLabel.setForeground(resources.antiflashWhite);
+        studentLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        studentLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+        studentPanel.add(studentLabel, BorderLayout.CENTER);
+
+        JLabel idLabel = new JLabel();
+        idLabel.setText("2230000");
+        idLabel.setFont(resources.montserratBold.deriveFont(15f));
+        idLabel.setForeground(resources.antiflashWhite);
+        idLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        idLabel.setVerticalAlignment(SwingConstants.CENTER);
+        studentPanel.add(idLabel, BorderLayout.SOUTH);
+
+
+        // !!! Name Text Field
+        gbc.gridy = 1;
+
+        JTextField nameTextField = new JTextField();
+        nameTextField.setFont(resources.montserrat.deriveFont(15f));
+        nameTextField.setText("Search by Name");
+        nameTextField.setColumns(14);
+        nameTextField.setForeground(Color.BLACK);
+        searchPanel.add(nameTextField, gbc);
+
+        // !!! ID Number Text Field
+        gbc.gridy = 2;
+        gbc.gridwidth = 1;
+        gbc.gridx = 0;
+
+        JTextField idTextField = new JTextField();
+        idTextField.setFont(resources.montserrat.deriveFont(15f));
+        idTextField.setText("Search by SLU ID Number");
+        idTextField.setColumns(14);
+        idTextField.setForeground(Color.BLACK);
+        searchPanel.add(idTextField, gbc);
+
+        // !!! Course Combo Box
+        gbc.gridx = 1;
+
+        String[] courses = {"BSCS","BSIT"}; // tentative array of courses
+
+        JComboBox<String> programComboBox = new JComboBox<>(courses);
+        programComboBox.setFont(resources.montserrat.deriveFont(15f));
+        programComboBox.setForeground(Color.BLACK);
+        searchPanel.add(programComboBox, gbc);
+
+        // !! Buttons Panel
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridBagLayout());
+        buttonsPanel.setBackground(resources.antiflashWhite);
+        buttonsPanel.setPreferredSize(new Dimension(500, 100));
+        containerPanel1.add(buttonsPanel, BorderLayout.SOUTH);
+
+        // !!! Buttons Panel Components
+        gbc.insets = new Insets(5,200,5,200);
+        gbc.weightx = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridwidth = 2;
+
+        // !!! Search Button
+        ImageIcon searchIcon = new ImageIcon("icons/person-search-icon-black.png");
+
+        gbc.gridy = 0;
+
+        JButton searchButton = new JButton();
+        searchButton.setText("Search");
+        searchButton.setIcon(searchIcon);
+        searchButton.setFont(resources.montserrat.deriveFont(15f));
+        searchButton.setOpaque(true);
+        searchButton.setBorderPainted(false);
+        searchButton.setBackground(resources.airSuperiorityBlue);
+        searchButton.setForeground(Color.BLACK);
+        buttonsPanel.add(searchButton, gbc);
+
+        // !!! Clear Button
+        ImageIcon clearIcon = new ImageIcon("icons/clear_all-icon-black.png");
+
+        gbc.gridy = 1;
+
+        JButton clearButton = new JButton();
+        clearButton.setText("Clear All");
+        clearButton.setIcon(clearIcon);
+        clearButton.setFont(resources.montserrat.deriveFont(15f));
+        clearButton.setOpaque(true);
+        clearButton.setBorderPainted(false);
+        clearButton.setBackground(resources.airSuperiorityBlue);
+        clearButton.setForeground(Color.BLACK);
+        buttonsPanel.add(clearButton, gbc);
+
+        // ! Container 2
+        JPanel containerPanel2 = new JPanel();
+        containerPanel2.setLayout(new BorderLayout());
+        containerPanel2.setBackground(resources.antiflashWhite);
+        containerPanel2.setPreferredSize(new Dimension(910,700));
+        torPanel.add(containerPanel2, "2");
+
+        // !! Container 2 Components
+
+
+        // Action Listeners
+        // FIXME: 9/17/2023
+        searchButton.addActionListener(e->{
+            cardLayout1.show(torPanel, "2");
+        });
+
+        return torPanel;
+    } // end of populateTorPanel
 
     /*
     To be removed.

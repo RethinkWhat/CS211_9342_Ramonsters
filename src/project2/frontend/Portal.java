72d89813f8b4ab211a732a98348c9235dd178942
Patrick Lacanilao <mpatricklacanilao@gmail.com>
2023-09-24 20:16:30 +0800

    Student Window

--- src/project2/frontend/Portal.java
+++ src/project2/frontend/Portal.java
@@ -1017,19 +1017,12 @@ public class Portal extends JFrame {
      * @Author: LACANILAO
      * TODO: add handling exceptions
      * TODO: add remove student frame
-<<<<<<< HEAD
-     * TODO: add logos
-=======
->>>>>>> abf2e494228305842d7fc2015e3bd71a2479ee6d
      *
      */
     private void addStudentFrame() {
         addStudentFrame = new JFrame("Add Student");
-<<<<<<< HEAD
-=======
         ImageIcon sluStudLogo = resources.scaleImage(resources.sluLogo, 25, 25);
         addStudentFrame.setIconImage(sluStudLogo.getImage());
->>>>>>> abf2e494228305842d7fc2015e3bd71a2479ee6d
         addStudentFrame.setSize(600, 500);
         addStudentFrame.setLocationRelativeTo(null);
         addStudentFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -1043,11 +1036,7 @@ public class Portal extends JFrame {
 
 
         ImageIcon logoIcon = new ImageIcon("icons/add-student-icon-black.png");
-<<<<<<< HEAD
-        ImageIcon scaledLogo = resources.scaleImage(logoIcon, 400, 400);
-=======
         ImageIcon scaledLogo = resources.scaleImage(logoIcon, 200, 200);
->>>>>>> abf2e494228305842d7fc2015e3bd71a2479ee6d
         JLabel logoLabel = new JLabel(scaledLogo);
 
         topPanel.setLayout(new BorderLayout());
@@ -1110,13 +1099,8 @@ public class Portal extends JFrame {
         bottomPanel.add(inputPanel);
 
         JPanel buttonPanel = new JPanel();
-<<<<<<< HEAD
-        JButton addButton = new JButton("Add");
-        JButton cancelButton = new JButton("Cancel");
-=======
         JButton addButton = new JButton("ADD");
         JButton cancelButton = new JButton("CANCEL");
->>>>>>> abf2e494228305842d7fc2015e3bd71a2479ee6d
 
         addButton.setFont(new Font("Montserrat Bold", Font.BOLD, 24));
         addButton.setBackground(resources.uranianBlue);
@@ -1138,11 +1122,6 @@ public class Portal extends JFrame {
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-<<<<<<< HEAD
-                // Handle the Add button click (add the student)
-                // Add student to data
-                addStudentFrame.dispose(); // Close the frame when done
-=======
                 String idText = idTextField.getText(); // can modify. just used for testing
                 String firstNameText = firstNameTextField.getText();
                 String lastNameText = lastNameTextField.getText();
@@ -1156,25 +1135,19 @@ public class Portal extends JFrame {
                     // Add student to data
                     addStudentFrame.dispose(); // Close the frame when done
                 }
->>>>>>> abf2e494228305842d7fc2015e3bd71a2479ee6d
             }
         });
 
         cancelButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-<<<<<<< HEAD
-                // Handle the Cancel button click (cancel adding a student)
-                // Close the frame
-                addStudentFrame.dispose();
-=======
->>>>>>> abf2e494228305842d7fc2015e3bd71a2479ee6d
             }
         });
 
         addStudentFrame.setContentPane(frameContent);
         addStudentFrame.setVisible(true);
     } // end of addStudentFrame
+
     /**
      * TODO: Documentation
      * @return

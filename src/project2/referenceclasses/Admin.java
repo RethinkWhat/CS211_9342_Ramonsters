9f3966d1ae22ffe82d9c6c405a1154e6ad68402f
Karl Jasper Urbiztondo <2233067@slu.edu.ph>
2023-10-01 15:42:56 +0800

    Added comments for StudentUtility and Admin in Reference class

--- src/project2/referenceclasses/Admin.java
+++ src/project2/referenceclasses/Admin.java
@@ -1,14 +1,23 @@
 package project2.referenceclasses;
 import project2.backend.Node;
 
+/**
+ * Allows for the creation of admin users
+ */
 public class Admin {
-
+    /**
+     * Private attributes for the admin methods to utilize
+     */
     //attributes
     private String firstName; //Represents the firstname.
     private String lastName; //Represents the last name.
     private String userName;// Represents user name.
     private String password;// Represents password.
     private String email;//Represents email.
+
+    /**
+     * Initializes the Admin class attributes when creating an object
+     */
 
     public Admin(String firstName, String lastName, String userName, String password, String email) { //constructor for all attributes.
         this.firstName = firstName;
@@ -17,10 +26,19 @@ public class Admin {
         this.password = password;
         this.email = email;
     }
+
+    /**
+     * Initializes userName and password to allow for a simplified creation of admin object
+     */
     public Admin(String userName, String password) {// constructor for username and password.
         this.userName = userName;
         this.password = password;
     }
+
+    /**
+     * Getter methods retrieve the private attribute values of the Admin class
+     * Setter methods modify the value of the private attributes
+     */
     public String getFirstName() {
         return firstName;
     }//getter for first name
@@ -60,12 +78,18 @@ public class Admin {
     public void setEmail(String email) {
         this.email = email;
     }// setter for email
-
+    /**
+     * Checks if the inputted details of the admin object matches the object in Node
+     * @return true, otherwise false
+     */
     //@Override
     public boolean equals(Node<Admin> obj) {
         return this.password.equals(obj.data.getPassword()) && this.userName.equalsIgnoreCase(obj.getData().getUserName());
     }
 
+    /**
+     * @return a formatted string of Admin object
+     */
     @Override
     public String toString() {
         return userName + "," + password;

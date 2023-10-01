e709937b0f9f0fb5e1e81ad21afc5c84a27b86d0
RAMON EMMIEL JASMIN <2230748@slu.edu.ph>
2023-10-01 14:22:30 +0800

    docs: Update documentation in the backend package

--- src/project2/backend/Node.java
+++ src/project2/backend/Node.java
@@ -1,39 +1,95 @@
 package project2.backend;
 
+/**
+ * A generic class representing a node in a linked list.
+ *
+ * @param <T> The type of data stored in the node.
+ */
 public class Node<T> {
+    /**
+     * The data stored in the node.
+     */
     public T data;
+
+    /**
+     * Reference to the next node in the linked list.
+     */
     public Node<T> next;
 
-    public Node(T data){
+    /**
+     * Constructs a new Node with the given data and sets the next reference to null.
+     *
+     * @param data The data to be stored in the node.
+     */
+    public Node(T data) {
         this.data = data;
         this.next = null;
     }
 
-    public Node(T data, Node<T> next){
+    /**
+     * Constructs a new Node with the given data and a reference to the next node.
+     *
+     * @param data The data to be stored in the node.
+     * @param next The reference to the next node in the linked list.
+     */
+    public Node(T data, Node<T> next) {
         this.data = data;
         this.next = next;
     }
 
+    /**
+     * Gets the data stored in the node.
+     *
+     * @return The data stored in the node.
+     */
     public T getData() {
         return data;
     }
 
+    /**
+     * Gets the reference to the next node in the linked list.
+     *
+     * @return The reference to the next node.
+     */
     public Node<T> getNext() {
         return next;
     }
 
+    /**
+     * Sets the data stored in the node.
+     *
+     * @param data The data to be stored in the node.
+     */
     public void setData(T data) {
         this.data = data;
     }
 
+    /**
+     * Sets the reference to the next node in the linked list.
+     *
+     * @param next The reference to the next node.
+     */
     public void setNext(Node<T> next) {
         this.next = next;
     }
 
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     * This method overrides the default behavior of the equals method in the Object class.
+     *
+     * @param obj The object to compare for equality.
+     * @return true if this object is the same as the obj argument; false otherwise.
+     */
+    @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
     }
 
+    /**
+     * Returns a string representation of the node's data.
+     *
+     * @return A string representation of the node's data.
+     */
     @Override
     public String toString() {
         return "" + this.data;
--- src/project2/backend/interfaces/ListInterface.java
+++ src/project2/backend/interfaces/ListInterface.java
@@ -2,10 +2,47 @@ package project2.backend.interfaces;
 
 import java.util.NoSuchElementException;
 
+/**
+ * An interface representing a generic list data structure.
+ *
+ * @param <T> The type of elements in the list.
+ */
 public interface ListInterface<T> {
+    /**
+     * Gets the size (number of elements) of the list.
+     *
+     * @return The size of the list.
+     */
     public int getSize();
+
+    /**
+     * Inserts an element into the list.
+     *
+     * @param data The data to be inserted into the list.
+     */
     public void insert(T data);
+
+    /**
+     * Retrieves an element from the list based on its data.
+     *
+     * @param data The data to search for in the list.
+     * @return The element with the specified data.
+     * @throws NoSuchElementException If the element with the specified data is not found.
+     */
     public T getElement(T data) throws NoSuchElementException;
+
+    /**
+     * Deletes an element from the list based on its data.
+     *
+     * @param data The data of the element to be deleted from the list.
+     */
     public void delete(T data);
+
+    /**
+     * Searches for an element in the list and returns its position/index.
+     *
+     * @param data The data to search for in the list.
+     * @return The position/index of the element with the specified data, or -1 if not found.
+     */
     public int search(T data);
 }
